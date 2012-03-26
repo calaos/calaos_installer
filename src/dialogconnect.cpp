05543b4ac93d1c4ea72fe0e7df711057e1b6ffd6
Raoul Hecky <raoul@calaos.fr>
2012-03-26 14:09:24 +0000

    Better wago connection window
    git-svn-id: http://dev.calaos.fr/calaos-svn/calaos_installer@2140 5480cf3d-c827-0410-bd00-b44130fca4db

--- src/dialogconnect.cpp
+++ src/dialogconnect.cpp
@@ -9,10 +9,10 @@ DialogConnect::DialogConnect(QWidget *parent): QDialog(parent),
         ui->setupUi(this);
 
         ui->ip_address->setText(ConfigOptions::Instance().getWagoHost());
-        ui->checkBoxCalaosFr->setChecked(ConfigOptions::Instance().useCalaosFr());
-        ui->groupProxy->setChecked(ConfigOptions::Instance().useCalaosServer());
-        ui->username->setText(ConfigOptions::Instance().getUsername());
-        ui->password->setText(ConfigOptions::Instance().getPassword());
+//        ui->checkBoxCalaosFr->setChecked(ConfigOptions::Instance().useCalaosFr());
+//        ui->groupProxy->setChecked(ConfigOptions::Instance().useCalaosServer());
+//        ui->username->setText(ConfigOptions::Instance().getUsername());
+        //ui->password->setText(ConfigOptions::Instance().getPassword());
 }
 
 DialogConnect::~DialogConnect()
@@ -35,12 +35,12 @@ void DialogConnect::changeEvent(QEvent *e)
 void DialogConnect::on_buttonBox_accepted()
 {
         ConfigOptions::Instance().setWagoHost(ui->ip_address->text());
-        ConfigOptions::Instance().setUseCalaosFr(ui->checkBoxCalaosFr->isChecked());
-        ConfigOptions::Instance().setUseCalaosServer(ui->groupProxy->isChecked());
-        ConfigOptions::Instance().setUsername(ui->username->text());
-        ConfigOptions::Instance().setPassword(ui->password->text());
+//        ConfigOptions::Instance().setUseCalaosFr(ui->checkBoxCalaosFr->isChecked());
+//        ConfigOptions::Instance().setUseCalaosServer(ui->groupProxy->isChecked());
+//        ConfigOptions::Instance().setUsername(ui->username->text());
+//        ConfigOptions::Instance().setPassword(ui->password->text());
 
-        WagoConnect::Instance().setUsername(ui->username->text());
-        WagoConnect::Instance().setPassword(ui->password->text());
-        WagoConnect::Instance().Connect(ui->ip_address->text(), ui->groupProxy->isChecked());
+//        WagoConnect::Instance().setUsername(ui->username->text());
+//        WagoConnect::Instance().setPassword(ui->password->text());
+        WagoConnect::Instance().Connect(ui->ip_address->text(), false);
 }
