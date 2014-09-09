6f525e631caa272c8990b1483a82bc6b646d4ae1
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2014-09-09 08:20:53 +0200

    Fix build script : deploy more shared libs with calaos_installer to enable launch on ubuntu 14.04

--- linux/update_compile_package.sh
+++ linux/update_compile_package.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-QTPATH="/home/$USER/Qt/5.3/gcc_64"
-QTVERSION="5.3.1"
+QTVERSION="5.3"
+QTPATH="/home/$USER/Qt/$QTVERSION/gcc_64"
+
 VERSION=`../calaos_installer --version | tail -1 | tr -d '\t' `
 
 echo "Creating package..."
@@ -9,20 +10,21 @@ mkdir -p calaos_installer_$VERSION/.lib
 
 cp ../calaos_installer calaos_installer_$VERSION/.calaos_installer.bin
 
-for l in libQt5DBus.so libQt5Gui.so libQt5Network.so libQt5Core.so libQt5Svg.so libQt5Xml.so
+for l in libQt5DBus.so.5 libQt5Gui.so.5 libQt5Network.so.5 libQt5Core.so.5 libQt5Svg.so.5 libQt5Xml.so.5 libQt5PrintSupport.so.5 libQt5Widgets.so.5 
 do
- 	cp $QTPATH/lib/$l.$QTVERSION calaos_installer_$VERSION/.lib/
+ 	cp $QTPATH/lib/$l calaos_installer_$VERSION/.lib/
 done
 
-#for l in /usr/lib/libssl.so.0.9.8 /usr/lib/libexpat.so.1 /usr/lib/libpng12.so.0 /usr/lib/libjpeg.so.62
-#do
-#	cp $l calaos_installer_$VERSION/.lib/
-#done
+for l in /usr/lib/libstdc++.so.6 /usr/lib/libssl.so.1.0.0 /usr/lib/libcrypto.so.1.0.0
+do
+	cp $l calaos_installer_$VERSION/.lib/
+done
 
 cp -R $QTPATH/plugins/iconengines calaos_installer_$VERSION
 cp -R $QTPATH/plugins/imageformats calaos_installer_$VERSION
+cp -R $QTPATH/plugins/platforms calaos_installer_$VERSION
 
-strip calaos_installer_$VERSION/.calaos_installer.bin calaos_installer_$VERSION/.lib/* calaos_installer_$VERSION/iconengines/* calaos_installer_$VERSION/imageformats/*
+strip calaos_installer_$VERSION/.calaos_installer.bin calaos_installer_$VERSION/.lib/* calaos_installer_$VERSION/iconengines/* calaos_installer_$VERSION/imageformats/* calaos_installer_$VERSION/platforms/*
 
 cat > calaos_installer_$VERSION/calaos_installer << "EOF"
 #!/bin/sh
