336b9e900113f5b0a5ac5cbf4fc7d6c0df58507b
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2016-11-07 23:52:50 +0100

    Add the right version number in versio.h

--- scripts/ci/funcs.sh
+++ scripts/ci/funcs.sh
@@ -16,7 +16,10 @@ function make_version()
 {
     echo "#ifndef VERSION__H" > $1/src/version.h
     echo "#define VERSION__H" >> $1/src/version.h
+
     echo "#define APP_VERSION \"$(get_version $1)\"" >> $1/src/version.h
+    echo "const char *calaos_installer_version = APP_VERSION" >> $1/src/version.h;
+
     echo "#endif" >> $1/src/version.h
 }
 
