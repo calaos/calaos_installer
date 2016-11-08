491a77157ddd3ed8415a81e863b4693fc02f9014
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2016-11-09 00:21:30 +0100

    Fix launch of Recipe script in docker

--- scripts/ci/linux/script.sh
+++ scripts/ci/linux/script.sh
@@ -2,7 +2,10 @@
 set -ev
 
 docker pull centos:6
-docker run --name buildvm centos:6 /bin/bash Recipe
+docker run -i -t --name buildvm -d centos:6 /bin/bash
+docker cp Recipe buildvm:Recipe
+docker exec -ti buildvm /Recipe
+
 docker cp buildvm:/calaos_instaler/calaos_installer.AppImage calaos_installer.AppImage
 
 ./script_win32.sh
