e57d4b92417ffd9f82105422ad65f860c3193615
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2016-11-09 21:19:52 +0100

    Really copy Recipe into docker

--- scripts/ci/linux/script.sh
+++ scripts/ci/linux/script.sh
@@ -3,7 +3,7 @@ set -ev
 
 docker pull centos:6
 docker run -i -t --name buildvm -d centos:6 /bin/bash
-docker cp Recipe buildvm:Recipe
+docker cp scripts/ci/linux/Recipe buildvm:Recipe
 docker exec -ti buildvm /Recipe
 
 docker cp buildvm:/calaos_instaler/calaos_installer.AppImage calaos_installer.AppImage
