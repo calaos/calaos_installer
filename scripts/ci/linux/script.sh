487bafd205fad8854b917851e7b95507ab4b6c1f
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2016-11-11 15:38:07 +0100

    Do not build linux AppImage with travis as it's not working yet

--- scripts/ci/linux/script.sh
+++ scripts/ci/linux/script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -ev
 
-docker pull centos:6
-docker run -i -t --name buildvm -d centos:6 /bin/bash
-docker cp scripts/ci/linux/Recipe buildvm:Recipe
-docker exec -ti buildvm /Recipe
-
-docker cp buildvm:/calaos_instaler/calaos_installer.AppImage calaos_installer.AppImage
-
 ./script_win32.sh
+
+#docker pull centos:6
+#docker run -i -t --name buildvm -d centos:6 /bin/bash
+#docker cp scripts/ci/linux/Recipe buildvm:Recipe
+#docker exec -ti buildvm /Recipe
+#docker cp buildvm:/calaos_instaler/calaos_installer.AppImage calaos_installer.AppImage
+
+
