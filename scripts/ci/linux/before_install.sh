4732785c51de93cdc46dcb39e8c8f366c07ebe4e
Raoul Hecky <raoul.hecky@gmail.com>
2021-04-24 11:23:22 +0200

    fix travis and docker

--- scripts/ci/linux/before_install.sh
+++ scripts/ci/linux/before_install.sh
@@ -5,5 +5,6 @@ SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $SCRIPTDIR/../funcs.sh
 
 # Docker
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker pull calaos/winbuilder
 
