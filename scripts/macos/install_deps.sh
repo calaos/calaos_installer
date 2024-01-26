a6b84f5c4db9cc26e6f733b9ce23c7cd1d4ab8b6
Raoul Hecky <raoul.hecky@gmail.com>
2024-01-26 12:59:18 +0100

    manually build deps

--- scripts/macos/install_deps.sh
+++ scripts/macos/install_deps.sh
@@ -5,7 +5,29 @@ echo "install.sh"
 
 brew update > /dev/null
 brew install qt6
-brew install --HEAD extra-cmake-modules kde-karchive
+
+#get extra-cmake-modules and build it
+git clone https://invent.kde.org/frameworks/extra-cmake-modules.git
+cd extra-cmake-modules
+git checkout 5.114.0
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="$(brew --prefix qt6)" -DCMAKE_PREFIX_PATH="$(brew --prefix qt6)"
+make -j$(sysctl -n hw.physicalcpu)
+make install
+cd ../..
+
+#get karchive and build it
+git clone https://invent.kde.org/frameworks/karchive.git
+cd karchive
+git checkout 5.114.0
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="$(brew --prefix qt6)" -DCMAKE_PREFIX_PATH="$(brew --prefix qt6)" \
+        -DBUILD_TESTING=0 \
+		-DBUILD_WITH_QT6=ON \
+        -DQT_MAJOR_VERSION=6
+make -j$(sysctl -n hw.physicalcpu)
+make install
+cd ../..
 
 #get qtmqtt and build it
 git clone https://github.com/qt/qtmqtt.git
